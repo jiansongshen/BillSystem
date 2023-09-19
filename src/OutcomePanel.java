38076de36ef41b36b31fee492f1f45d47eb0165b
jiansongshen <98138652+jiansongshen@users.noreply.github.com>
2023-09-19 14:24:42 +0800

    Update OutcomePanel.java

--- src/OutcomePanel.java
+++ src/OutcomePanel.java
@@ -8,14 +8,17 @@ import java.util.Vector;
 /*
  * Author: Jiansong Shen
  * Version: 1.0
+ * 支出界面
  */
 public class OutcomePanel extends JPanel implements Runnable {
-
+    //本次运行中产生的记录
     Vector<Record> vector = new Vector<>();
     Record record = null;
+    //本次软件启动中的支出
     double outcome = 0;
 
     OutcomePanel() {
+        //初始化
         this.record = new Record();
         this.setPreferredSize(new Dimension(660, 100));
 
@@ -27,13 +30,17 @@ public class OutcomePanel extends JPanel implements Runnable {
 
         record.setDate(now.toString());
 
+        //各种模块的初始化
+        //按钮初始化
         JButton button1 = new JButton("吃饭");
         JButton button2 = new JButton("娱乐");
         JButton button3 = new JButton("学习工作");
         JButton deleteRecordButton = new JButton("删除上一条记录");
 
+        //文本框初始化，用于输入金额
         TextField textField = new TextField();
 
+        //标签初始化
         JLabel jLabel = new JLabel("开销类型:");
         JLabel jLabel_return_message = new JLabel("");
         JLabel titleLabel = new JLabel("BILL SYSTEM");
@@ -70,19 +77,24 @@ public class OutcomePanel extends JPanel implements Runnable {
         this.add(button2);
         this.add(button3);
 
+        //设置按下按钮的行为
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //接收输入
                 try {
                     outcome = Double.parseDouble(textField.getText());
                 } catch (NumberFormatException ex) {
                     textField.setText("请输入数字");
                 }
+                
                 if (outcome != 0) {
+                    //用于初始化record的日期类
                     Date temp = new Date();
                     temp.getTime();
                     record = new Record(temp.toString(), outcome, "1");
                     vector.add(record);
+                    //用户输入完后清空输入栏
                 }
             }
         });
